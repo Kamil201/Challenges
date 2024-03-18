4990946b0d09c048a9a60b753ce72dc2e678c0cf
Kamil Lewandowski <kamlew555@gmail.com>
2024-03-18 20:18:05 +0100

    create functions adding tasks/rendering/saving in local storage

--- ToDo-App/assets/script.js
+++ ToDo-App/assets/script.js
@@ -1,3 +1,86 @@
+const loadTasksFromStorage = () => {
+	const storedTasks = localStorage.getItem("tasks");
+	return storedTasks ? JSON.parse(storedTasks) : [];
+};
+
+const saveTasksToStorage = () => {
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+	render("body");
+};
+
+const addTask = (newTaskText) => {
+	const newTask = {
+		text: newTaskText,
+		done: false,
+		timestamp: Date.now(),
+	};
+	tasks[tasks.length] = newTask;
+	
+
+	if (newTaskText.trim().length === 0) {
+		return;
+	}
+
+	render("body");
+	saveTasksToStorage();
+};
+
+const renderButton = (label, className) => {
+	const button = document.createElement("button");
+	button.textContent = label;
+	button.className = className;
+
+	return button;
+};
+
+const renderInput = (placeholder) => {
+	const input = document.createElement("input");
+	input.placeholder = placeholder;
+	return input;
+};
+
+const renderTaskElement = (task) => {
+	const li = document.createElement("li");
+	li.innerText = task.text;
+};
+
+const renderTasks = () => {
+	const taskContainer = document.createElement("div");
+	const ul = document.createElement("ul");
+
+	tasks && tasks.length > 0
+		? tasks.forEach((task) => ul.appendChild(createTaskElement(task.text)))
+		: ul.appendChild(createTaskElement(tasks));
+
+	function createTaskElement(text) {
+		const li = document.createElement("li");
+		li.textContent = text;
+		return li;
+	}
+
+	taskContainer.appendChild(ul);
+	return taskContainer;
+};
+
+const renderForm = () => {
+	const formContainer = document.createElement("div");
+
+	const form = document.createElement("form");
+
+	const inputElement = renderInput("Enter new task");
+	const buttonElement = renderButton("Add task");
+
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+		addTask(inputElement.value);
+	});
+
+	form.appendChild(inputElement);
+	form.appendChild(buttonElement);
+
+	formContainer.append(form);
+	return formContainer;
+};
 
 const render = (containerSelector) => {
 	const container = document.querySelector(containerSelector);
@@ -9,6 +92,13 @@ const render = (containerSelector) => {
 		return;
 	}
 
+	container.innerHTML = "";
+
+	const form = renderForm();
+	const taskContainer = renderTasks();
+	container.appendChild(form);
+	container.appendChild(taskContainer);
 };
 
-render(".todo");
+const tasks = loadTasksFromStorage();
+render("body");
